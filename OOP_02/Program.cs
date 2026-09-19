5576dc6a615e91dc8020c4b881c8c15c64767c1e
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-09-19 18:16:14 +0300

    Part1 Question1

--- OOP_02/Program.cs
+++ OOP_02/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
- 
+            #region Part1 Question1
+            ////a)Class is a refernce type data type that supports inheritance and usualy used for the big data
+            ////b)Struct is a value type data type that doesn't supports inheritance and usualy used for the small data
+            #endregion
         }
     }
 }
