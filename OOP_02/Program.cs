482e3d25a09162e1894fba32036976c40411bb4d
mohandabdelshafy443-eng <mohandabdelshafy443@gmail.com>
2026-09-19 18:22:34 +0300

    Part1 Question2

--- OOP_02/Program.cs
+++ OOP_02/Program.cs
@@ -8,6 +8,12 @@
             ////a)Class is a refernce type data type that supports inheritance and usualy used for the big data
             ////b)Struct is a value type data type that doesn't supports inheritance and usualy used for the small data
             #endregion
+            #region Part1 Question2
+            ////a)The paret class is Shipment
+            ////b)The child class is ExpressShipment
+            ////c)It inherits the property TrackingCode(besides the baisc object functions)
+            ////d)The inheritance reduces repition which reduces bugs and makes the code easier to mantain instead of having only a base class
+            #endregion
         }
     }
 }
